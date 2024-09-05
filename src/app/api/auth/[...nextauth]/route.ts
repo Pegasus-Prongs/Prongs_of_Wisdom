6a254c9003a986f5c9f59419846ca4fc97627294
Pegasus Prongs <PegasusProngs@gmail.com>
2024-09-06 02:55:51 +0700

    Update authentication

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth';
+import { NextApiRequest, NextApiResponse } from 'next';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import connectToDatabase from '@/lib/mongodb';
 import User from '@/models/User';
@@ -7,18 +8,18 @@ import bcrypt from 'bcryptjs';
 export const authOptions = {
   providers: [
     CredentialsProvider({
-      name: 'Credentials',
+      name: 'credentials',
       credentials: {
         username: { label: 'Username', type: 'text' },
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials) {
-        console.log( credentials)
+        console.log(credentials);
         await connectToDatabase();
 
         const user = await User.findOne({ username: credentials?.username });
-        console.log("User===>", user);
-        if (user && bcrypt.compareSync(credentials?.password|| '', user.password)) {
+        console.log('User===>', user);
+        if (user && bcrypt.compareSync(credentials?.password || '', user.password)) {
           return { id: user._id, name: user.username };
         } else {
           return null;
@@ -34,18 +35,23 @@ export const authOptions = {
   session: {
     strategy: 'jwt',
   },
+  
+};
+
+export async function GET(request: NextApiRequest, response: NextApiResponse) {
+  return NextAuth(request, response, authOptions);
+}
+
+export async function POST(request: NextApiRequest, response: NextApiResponse) {
+  return NextAuth(request, response, authOptions);
+}
+
+export const authOptionsWithRedirect = {
+  ...authOptions,
   callbacks: {
-    async redirect({ url, baseUrl }) {
+    async redirect({ url, baseUrl }: { url: string; baseUrl: string }) {
       // Redirect to home page or any specific page upon successful sign-in
       return url.startsWith(baseUrl) ? url : baseUrl;
     },
   },
-};
-
-export async function GET(request: Request) {
-  return NextAuth(request, authOptions);
-}
-
-export async function POST(request: Request) {
-  return NextAuth(request, authOptions);
 }
