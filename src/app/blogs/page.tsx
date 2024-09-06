98b639e4cc95faf20cdc644a18c0b300c8b387fc
Pegasus Prongs <PegasusProngs@gmail.com>
2024-09-06 15:34:12 +0700

    update bloglist has suspense

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,35 +1,27 @@
-import PostCard from '@/components/PostCard';
-import axios from 'axios';
-import React from 'react';
-interface IBlogPost {
-    id: string;
-    title: string;
-    content: string;
-}
+import { PostRow } from '@/components/PostRow';
+import SearchBar from '@/components/SearchBar';
+import { BlogPostsSkeleton } from '@/components/Skeletons';
 
-async function fetchBlogList(): Promise<IBlogPost[]> {
-    try {
-        const response = await axios.get('http://localhost:3000/api/blog');
-        return response.data; // Access the data from the response
-    } catch (error) {
-        console.error('Error fetching blog list:', error);
-        return []; // Return an empty array in case of an error
+import React, { Suspense } from 'react';
+
+
+export default async function BlogList({
+    searchParams,
+}: {
+    searchParams?: {
+        query: string; // Search query from the URL query parameters
     }
-}
-
-export default async function BlogList() {
-    const blogPosts = await fetchBlogList();
+}) {
+   const query = searchParams?.query || '';
     return (
         <div className='p-6 max-w-6xl mx-auto'>
+            <SearchBar className="m-6" />
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+                <Suspense key={query} fallback={<BlogPostsSkeleton />}>
+                <PostRow query = {query}></PostRow>
+                    
+                </Suspense>
 
-                {blogPosts.length > 0 ? (
-                    blogPosts.map((post: IBlogPost) => (
-                        <PostCard key={post.id} {...post} />
-                    ))
-                ) : (
-                    <p>No blog posts available.</p>
-                )}
             </div>
         </div>
     )
