b542f6c85dbaaabe2a8efe83432c90beaae6e3df
Pegasus Prongs <PegasusProngs@gmail.com>
2024-09-06 15:33:49 +0700

    Fix bugs in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
@@ -14,12 +14,17 @@ export default function Navbar() {
       setIsDropdownOpen(false);
     }
   }, 100);
+  
+  useEffect(() => {
+    setIsOpen(false);
+    setIsDropdownOpen(false);
+  }, [pathname])
 
   return (
     <nav className="bg-blue-500 text-white shadow-md">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
             <button
               type="button"
               className="inline-flex items-center justify-center p-2 text-gray-400 hover:text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
@@ -39,13 +44,13 @@ export default function Navbar() {
               )}
             </button>
           </div>
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+          <div className="flex-1 flex items-center justify-center md:items-stretch md:justify-start">
             <div className="flex-shrink-0">
               <Link href="/" className="text-2xl font-bold">
                 Prongs Of Wisdom
               </Link>
             </div>
-            <div className="hidden sm:flex sm:ml-6">
+            <div className="hidden md:flex md:ml-6">
               <div className="flex space-x-4">
                 <Link href="/" className={`text-gray-300 hover:bg-blue-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${pathname === '/' ? 'bg-blue-700' : ''}`}>
                   Home
@@ -90,26 +95,27 @@ export default function Navbar() {
 
               </div>
             </div>
+            <div className="absolute inset-y-0 right-0 flex items-center space-x-4 hidden md:flex">
+              <Link
+                href="/auth/signin"
+                className=" bg-gradient-to-r border-2 border-white from-blue-400 to-blue-600 text-white px-4 py-2 rounded-full shadow-md hover:shadow-lg hover:scale-105 transition-transform duration-200 ease-in-out"
+              >
+                Log In
+              </Link>
+              <Link
+                href="/auth/signup"
+                className="bg-gradient-to-r border-2 border-white from-teal-400 to-cyan-500 text-white px-4 py-2 rounded-full shadow-md hover:shadow-lg hover:scale-105 transition-transform duration-200 ease-in-out"
+              >
+                Sign Up
+              </Link>
+            </div>
           </div>
           {/* Right-Side Buttons */}
-          <div className="absolute inset-y-0 right-0 flex items-center space-x-4">
-            <Link
-              href="/auth/signin"
-              className=" bg-gradient-to-r border-2 border-white from-blue-400 to-blue-600 text-white px-4 py-2 rounded-full shadow-md hover:shadow-lg hover:scale-105 transition-transform duration-200 ease-in-out"
-            >
-              Log In
-            </Link>
-            <Link
-              href="/auth/signup"
-              className="bg-gradient-to-r border-2 border-white from-teal-400 to-cyan-500 text-white px-4 py-2 rounded-full shadow-md hover:shadow-lg hover:scale-105 transition-transform duration-200 ease-in-out"
-            >
-              Sign Up
-            </Link>
-          </div>
+
 
         </div>
       </div>
-      <div className={`${isOpen ? 'block' : 'hidden'} sm:hidden`} id="mobile-menu">
+      <div className={`${isOpen ? 'block' : 'hidden'} md:hidden`} id="mobile-menu">
         <div className="px-2 pt-2 pb-3 space-y-1">
           <Link href="/" className="text-gray-300 hover:bg-blue-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
             Home
