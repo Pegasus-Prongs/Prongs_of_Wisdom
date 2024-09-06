e67b905d638a5439ab7e15c09454ed276bc6cbde
Pegasus Prongs <PegasusProngs@gmail.com>
2024-09-06 15:33:25 +0700

    update postcard to show the html

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -18,9 +18,7 @@ const PostCard  = ({ id, title, content}: PostCardProps) => {
             <Link href={`/blog/${id}`} className="block text-2xl font-semibold text-gray-900 hover:text-blue-600 transition-colors duration-300">
                 {title}
             </Link>
-            <p className="mt-2 mb-4 text-gray-700 text-sm">
-                {truncateContent(content, 300)}
-            </p>
+            <div className="mt-2 mb-4 text-gray-700 text-sm" dangerouslySetInnerHTML={{ __html: truncateContent(content, 300) }} />
             <Link href={`/blog/${id}`} className="absolute bottom-4 right-6 text-blue-500 hover:underline font-medium text-sm">
                 Read more
             </Link>
