893989f35798c28ac03289d36fa26588aafdb6f6
Pegasus Prongs <PegasusProngs@gmail.com>
2024-09-13 01:21:19 +0700

    remove carousel and add intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,21 @@
-import React, {Suspense} from "react";
+import React, { Suspense } from "react";
 import Carousel from "@/components/Carousel";
 import CarouselItem from "@/components/CarouselItem";
 import Analysis from "./sections/Analysis";
 import { PopularLatestPosts } from "./sections/PopularLatestPosts";
 import TopUsers from "./sections/TopUsers";
+import EaseOutAnimation from "@/components/animations/EaseOutAnimation";
+import Introduction from "./sections/Introduction";
 export default function Home() {
   return (
     <>
       <main className="max-w-6xl m-auto flex flex-col items-center justify-between p-12">
-        <Carousel>
-          <CarouselItem>
-            <img src="/slider_bg.png" alt="Slide 1" className="w-full h-full object-cover" />
-          </CarouselItem>
-          <CarouselItem>
-            <img src="/slider_bg.png" alt="Slide 2" className="w-full h-full object-cover" />
-          </CarouselItem>
-          <CarouselItem>
-            <img src="/slider_bg.png" alt="Slide 3" className="w-full h-full object-cover" />
-          </CarouselItem>
-        </Carousel>
-        <PopularLatestPosts />
+        
+        <Introduction></Introduction>
+        <EaseOutAnimation>
+          <PopularLatestPosts />
+        </EaseOutAnimation>
+
         <Analysis />
         <TopUsers />
       </main>
