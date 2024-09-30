37b78db7a490c745a7c9ac5125ef3324ccf741f2
Pegasus Prongs <PegasusProngs@gmail.com>
2024-09-30 12:26:13 +0700

    remove unused import declarations

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -5,7 +5,6 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from 'next/navigation';
 
 import { auth, googleProvider, db } from "@/lib/firebase";
-import { adminDb } from '@/lib/firebase_admin'; // Import admin Firestore
 import { signInWithPopup } from "firebase/auth";
 import { doc, setDoc, getDoc } from "firebase/firestore";
 import { FcGoogle } from 'react-icons/fc';
