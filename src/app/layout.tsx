eba8ac1c67e6c046b02af09ff7b4740bea99df11
Pegasus Prongs <PegasusProngs@gmail.com>
2024-10-01 03:33:35 +0700

    add UserProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from '@/components/Navbar'; // Adjust path as necessary
 import Footer from "@/components/Footer";
+import { UserProvider } from "@/context/UserContext";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,9 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} min-h-screen flex flex-col justify-between bg-gradient-to-b from-white to-blue-100`}>
-      <Navbar />
-      <div className="flex-grow">{children} </div>
-      <Footer />
+        <UserProvider>
+          <Navbar />
+          <div className="flex-grow">{children} </div>
+          <Footer />
+        </UserProvider>
       </body>
     </html>
   );
